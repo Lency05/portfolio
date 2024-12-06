4c6490a7bd4f2fd2363ae5bd2218c1c5834472f0
Lency Preethi Suresh <95897208+Lency05@users.noreply.github.com>
2024-12-06 14:36:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -46,3 +46,20 @@ function eraseText(){
 }
 
 window.onload = typeWriter
+
+// About Section
+ 
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents  = document.getElementsByClassName("tab-contents");
+
+function opentab(event,tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+    
